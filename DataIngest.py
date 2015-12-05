4785673f5386905aea8e4e16efab174eb200ff05
jbocharov <john@voicebase.com>
2015-12-05 08:59:19 -0800

    Add support for and example of local sink

--- DataIngest.py
+++ DataIngest.py
@@ -3,6 +3,7 @@ import ConfigParser
 
 import TwitterDataIngestSource
 import S3DataIngestSink
+import LocalDataIngestSink
 
 
 def main():
@@ -29,6 +30,8 @@ class DataIngest:
 
   def start(self):
     if 'Twitter' in self.config.sections():
+      print 'Creating Twiter source (found [Twitter] section)'
+
       twitter_config = dict(self.config.items('Twitter'))
       twitter_source = TwitterDataIngestSource.TwitterDataIngestSource(
         twitter_config
@@ -40,18 +43,32 @@ class DataIngest:
       print "Skipping Twitter since config has no [Twitter] section"
 
     if 'Facebook' in self.config.sections():
+      print 'Creating Facebook source (found [Facebook] section)'
+
       facebook_config = dict(self.config.items('Facebook'))
       print facebook_config
     else:
       print "Skipping Facebook since config has no [Facebook] section" 
 
     if 'S3' in self.config.sections():
+      print 'Creating Local sink (found [S3] section)'
+
       s3_config = dict(self.config.items('S3'))
       s3_sink = S3DataIngestSink.S3DataIngestSink(s3_config)
 
       self.sinks.append(s3_sink)
     else:
       print "Skipping S3 since config has no [S3] section"
+
+    if 'Local' in self.config.sections():
+      print 'Creating Local sink (found [Local] section)'
+
+      local_config = dict(self.config.items('Local'))
+      local_sink = LocalDataIngestSink.LocalDataIngestSink(local_config)
+
+      self.sinks.append(local_sink)
+    else:
+      print "Skipping Local since config has no [Local] section"
 
     self.sinks[0].write(self.sources[0])
 
