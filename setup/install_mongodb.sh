00e63bdd3f90094e3b4bf5a12cea24eb8876afcf
Andrea Soto <andreasoto83@gmail.com>
2016-03-19 20:32:26 -0500

    Add script to install Mongo DB

---
+++ setup/install_mongodb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install mongo
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+# Pin a specific version of MongoDB.
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+# Start mongo 
+sudo service mongod start
